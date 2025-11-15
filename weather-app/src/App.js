9acff0dadd3d7f70ed9e89bfa25fc68feb996e17
eyeej <eyee.joy@gmail.com>
2025-11-15 19:36:55 +1100

    apikey included

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import WeatherSearch from './weathersearch'
+import WeatherForecast from './WeatherForecast';
 import './App.css';
 import './weathersearch.css';
 import './FormattedDate';
-import WeatherForecast from './WeatherForecast';
 
 export default function App() {
   return (
       <div className="App">
       <div className="container">
         <WeatherSearch />
+        <WeatherForecast />
       <footer>
         This Project was Coded by{" "} <a href="https://github.com/eyeej">Emma Yee-Joy</a> and is open-sourced on{" "}  
         <a href="https://github.com/eyeej/Weather-React">Github</a>{" "} and is{" "} hosted on <a href="https://shecodes-react-weather-hw.netlify.app/">Netlify</a>
--- weather-app/src/WeatherForecast.js
+++ weather-app/src/WeatherForecast.js
@@ -3,6 +3,8 @@ import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
+let apiKey = "97f8e93f00107773f88eafd933ce86b7";
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=74&lon=40.7&appid=${apiKey}`;
   return (
     <div className="WeatherForecast">
       <div className="row">

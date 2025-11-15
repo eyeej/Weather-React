ac8525e9a9cfa234dd2cf4d0cee99d8da50aa215
eyeej <eyee.joy@gmail.com>
2025-11-15 13:41:36 +1100

    weather app project

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,13 +1,16 @@
 import WeatherSearch from './weathersearch'
 import './styles.css';
 
-
 function App() {
   return (
-    <div className="App">
+      <div className="App">
       <header className="App-header">
         <WeatherSearch />
       </header>
+      <h1>Weather App</h1>
+      <footer>
+        This Project was Coded by Emma Yee-Joy and is open-sourced on <a href="https://github.com/eyeej">Github</a>
+      </footer>
     </div>
   );
 }
--- weather-app/src/weathersearch.js
+++ weather-app/src/weathersearch.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./styles.css";
 
+
 function WeatherSearch() {
   const [city, setCity] = useState("");
   const [loaded, setLoaded] = useState(false);
